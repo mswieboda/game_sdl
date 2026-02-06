4e20b55c033812c97b16109bceafb8664911023c
Matt Swieboda <mswieboda@users.noreply.github.com>
2026-02-05 22:24:18 -0600

    adds sprite class, texture loading pattern

--- examples/sprite.cr
+++ examples/sprite.cr
@@ -1,4 +1,4 @@
-require "../src/game_sdl" # Assuming game_sdl.cr is the main entry point for GSDL
+require "../src/game_sdl"
 
 module GameEx
   WIDTH = 800
@@ -7,13 +7,14 @@ module GameEx
   class Game < GSDL::Game
     def initialize
       super(title: "GSDL::TemplateManager Ex", width: WIDTH, height: HEIGHT)
+    end
 
+    def init
+      super
       @scene_manager = SceneManager.new
     end
 
     def load_textures
-      # Load the player texture
-      # The path is relative to the executable's working directory
       GSDL::TextureManager.load("player", "./assets/gfx/player.png")
     end
   end
@@ -29,23 +30,20 @@ module GameEx
   end
 
   class StartScene < GSDL::Scene
+    @sprite : GSDL::Sprite
+
     def initialize
       super(:start)
+
+      @sprite = GSDL::Sprite.new("player", 0, 0)
+      @sprite.center(WIDTH, HEIGHT)
     end
 
     def update(frame_time)
     end
 
     def draw(renderer : SDL3::Renderer)
-      # Get the texture dimensions
-      player_texture = GSDL::TextureManager.get("player")
-      player_width, player_height = player_texture.size
-
-      # center of the screen
-      x = (WIDTH - player_width) // 2
-      y = (HEIGHT - player_height) // 2
-
-      renderer.render_texture(texture: player_texture, x: x, y: y)
+      @sprite.draw(renderer)
     end
   end
 
---
+++ src/gsdl/gfx/sprite.cr
@@ -0,0 +1,32 @@
+module GSDL
+  class Sprite
+    property x : Float32
+    property y : Float32
+    getter texture : SDL3::Texture
+
+    @key : String
+
+    delegate size, to: @texture
+
+    def initialize(@key, @x, @y)
+      @texture = TextureManager.get(@key)
+    end
+
+    def width : Float32
+      size[0]
+    end
+
+    def height : Float32
+      size[1]
+    end
+
+    def center(width : Int32 | Float32, height : Int32 | Float32)
+      @x = (width - self.width) // 2
+      @y = (height - self.height) // 2
+    end
+
+    def draw(renderer : SDL3::Renderer)
+      renderer.render_texture(texture: texture, x: x, y: y)
+    end
+  end
+end
