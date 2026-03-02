64fa0890707e923b76f0dedbdb917611c8ae67d6
Matt Swieboda <mswieboda@users.noreply.github.com>
2026-03-01 22:17:14 -0600

    reorg camera file

--- src/gsdl/core/camera.cr
+++ src/gsdl/core/camera.cr
@@ -13,37 +13,65 @@ module GSDL
 
     property type : Type = Type::CenterOnTarget
 
+    property offset_x : Float32 = 0_f32
+    property offset_y : Float32 = 0_f32
+
+    property tweens : Array(Tween) = [] of Tween
+
+    property width : Num
+    property height : Num
+
+    # Target coordinates
+    property target_x : Num? = nil
+    property target_y : Num? = nil
+
+    # Boundary configuration
+    property boundary_x : Num = 0_f32
+    property boundary_y : Num = 0_f32
+    property boundary_width : Num = 0_f32
+    property boundary_height : Num = 0_f32
+
+    # Input movement configuration
+    property speed : Num = 300_f32
+    property input_up : Symbol = :camera_up
+    property input_down : Symbol = :camera_down
+    property input_left : Symbol = :camera_left
+    property input_right : Symbol = :camera_right
+
     @zoom : Float32 = 1.0_f32
-
-    def zoom
-      @zoom
-    end
-
-    def zoom=(new_zoom : Float32)
-      return if @zoom == new_zoom
-      
-      # Calculate the world coordinates of the center of the current camera view
-      center_x = @x + (@width / (2_f32 * @zoom))
-      center_y = @y + (@height / (2_f32 * @zoom))
-
-      @zoom = new_zoom
-
-      # Update @x and @y so that center_x and center_y are still at the center of the view
-      @x = center_x - (@width / (2_f32 * @zoom))
-      @y = center_y - (@height / (2_f32 * @zoom))
-    end
-
     @x : Float32 = 0_f32
     @y : Float32 = 0_f32
 
-    property offset_x : Float32 = 0_f32
-    property offset_y : Float32 = 0_f32
-    
-    property tweens : Array(Tween) = [] of Tween
+    def initialize(@width : Num, @height : Num)
+    end
+
+    def look_at(tx : Num, ty : Num)
+      @target_x = tx
+      @target_y = ty
+    end
+
+    def look_at(sprite : SpriteBase)
+      look_at(sprite.x, sprite.y)
+    end
+
+    def set_boundary(x : Num, y : Num, width : Num, height : Num)
+      @boundary_x = x
+      @boundary_y = y
+      @boundary_width = width
+      @boundary_height = height
+    end
+
+    def set_boundary(rect : Rect | FRect)
+      set_boundary(rect.x, rect.y, rect.width, rect.height)
+    end
+
+    def set_boundary(tile_map : TileMap)
+      set_boundary(0, 0, tile_map.width, tile_map.height)
+    end
 
     def z_index : Int32; 0; end
     def z_index=(z_index : Int32); end
-    
+
     def scale : Tuple(Num, Num)
       {@zoom, @zoom}
     end
@@ -74,35 +102,30 @@ module GSDL
       @y = y.to_f32
     end
 
-    property width : Num
-    property height : Num
+    def zoom
+      @zoom
+    end
 
-    # Target coordinates
-    property target_x : Num? = nil
-    property target_y : Num? = nil
+    def zoom=(new_zoom : Float32)
+      return if @zoom == new_zoom
 
-    # Boundary configuration
-    property boundary_x : Num = 0_f32
-    property boundary_y : Num = 0_f32
-    property boundary_width : Num = 0_f32
-    property boundary_height : Num = 0_f32
+      # Calculate the world coordinates of the center of the current camera view
+      center_x = @x + (@width / (2_f32 * @zoom))
+      center_y = @y + (@height / (2_f32 * @zoom))
 
-    # Input movement configuration
-    property speed : Num = 300_f32
-    property input_up : Symbol = :camera_up
-    property input_down : Symbol = :camera_down
-    property input_left : Symbol = :camera_left
-    property input_right : Symbol = :camera_right
+      @zoom = new_zoom
 
-    def initialize(@width : Num, @height : Num)
+      # Update @x and @y so that center_x and center_y are still at the center of the view
+      @x = center_x - (@width / (2_f32 * @zoom))
+      @y = center_y - (@height / (2_f32 * @zoom))
     end
 
     def shake(duration : Float32, intensity : Float32 = 10_f32)
       @offset_x = 0_f32
       @offset_y = 0_f32
-      
+
       steps = [] of Hash(String, Tween::SequenceValue)
-      
+
       num_steps = (duration / 0.05).to_i
       num_steps = 1 if num_steps < 1
       step_duration = duration / num_steps
@@ -111,7 +134,7 @@ module GSDL
         current_intensity = intensity * (1.0_f32 - (i.to_f32 / num_steps.to_f32))
         dx = (Random.rand * 2.0 - 1.0) * current_intensity
         dy = (Random.rand * 2.0 - 1.0) * current_intensity
-        
+
         steps << {
           "duration" => step_duration.as(Tween::SequenceValue),
           "offset_x" => dx.as(Tween::SequenceValue),
@@ -141,30 +164,6 @@ module GSDL
       when Type::Manual
         update_manual_movement(dt)
       end
-    end
-
-    def look_at(tx : Num, ty : Num)
-      @target_x = tx
-      @target_y = ty
-    end
-
-    def look_at(sprite : SpriteBase)
-      look_at(sprite.x, sprite.y)
-    end
-
-    def set_boundary(x : Num, y : Num, width : Num, height : Num)
-      @boundary_x = x
-      @boundary_y = y
-      @boundary_width = width
-      @boundary_height = height
-    end
-
-    def set_boundary(rect : Rect | FRect)
-      set_boundary(rect.x, rect.y, rect.width, rect.height)
-    end
-
-    def set_boundary(tile_map : TileMap)
-      set_boundary(0, 0, tile_map.width, tile_map.height)
     end
 
     private def update_center_on_target
