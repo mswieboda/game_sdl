595f62637878e23dff9cbfd13c70e65882b196f8
Matt Swieboda <mswieboda@users.noreply.github.com>
2026-03-11 22:07:28 -0500

    adds tween presets

---
+++ examples/tween_presets.cr
@@ -0,0 +1,189 @@
+require "../src/game_sdl"
+
+# This example showcases the Tween presets in GSDL.
+# Tweens can be used on any Tweenable object (Sprites, Shapes, etc.)
+# to animate properties like x, y, scale, and color/tint over time.
+
+module TweenPresetsEx
+  WIDTH = 800
+  HEIGHT = 600
+
+  class Game < GSDL::Game
+    def initialize
+      super(title: "Tween Presets Example", width: WIDTH, height: HEIGHT)
+    end
+
+    def init
+      GSDL::Events.esc_exits = true
+      @scene_manager = SceneManager.new
+    end
+
+    def load_default_font
+      "fonts/PressStart2P.ttf"
+    end
+
+    def load_textures
+      [{"ship", "gfx/ship.png"}]
+    end
+  end
+
+  class SceneManager < GSDL::SceneManager
+    def initialize
+      super
+      @scene = TweenScene.new
+    end
+  end
+
+  class TweenScene < GSDL::Scene
+    @active_info : GSDL::Text
+    @objects : Array(GSDL::Tweenable)
+    @active_index = 0
+
+    def initialize
+      super(:tween)
+
+      @objects = [] of GSDL::Tweenable
+
+      text_origin = {0.5_f32, 0_f32}
+      origin = {0.5_f32, 0.5_f32}
+
+      text = GSDL::Text.new(
+        text: "TAB toggles active object\n\n1: Flash   2: Pulse\n3: Spin    4: Shake",
+        origin: text_origin,
+        align: GSDL::Font::Align::Center,
+        color: GSDL::Color::Lime
+      )
+      text.x = WIDTH / 2_f32
+      text.y = 32
+
+      text_rotated = GSDL::TextRotated.new(
+        text: "ROTATED TEXT",
+        origin: origin,
+        color: GSDL::Color::Gold
+      )
+      text_rotated.x = WIDTH / 2_f32
+      text_rotated.y = HEIGHT - 128
+
+      @active_info = GSDL::Text.new(
+        text: "Active: Sprite",
+        origin: text_origin,
+        color: GSDL::Color::Yellow
+      )
+      @active_info.x = WIDTH / 2_f32
+      @active_info.y = text.y + text.height + 16
+
+      # Create a sprite to tween
+      sprite = GSDL::Sprite.new(
+        key: "ship",
+        origin: origin,
+        source_rect: GSDL::FRect.new(w: 128)
+      )
+      sprite.center(width: WIDTH, height: HEIGHT)
+      @objects << sprite
+
+      @objects << GSDL::Box.new(
+        width: 100,
+        height: 100,
+        x: WIDTH / 4_f32,
+        y: HEIGHT / 2_f32,
+        color: GSDL::Color::Crimson,
+        origin: origin,
+        border_radius: 10
+      )
+
+      @objects << GSDL::Oval.new(
+        x: (WIDTH * 3) / 4_f32,
+        y: HEIGHT / 2_f32,
+        radius_x: 64,
+        radius_y: 96,
+        color: GSDL::Color::Cyan,
+        origin: origin
+      )
+
+      @objects << GSDL::Pie.new(
+        x: (WIDTH * 3) / 4_f32,
+        y: HEIGHT / 2_f32 + 128,
+        radius: 96,
+        color: GSDL::Color::Indigo,
+        draw_mode: GSDL::Shape::DrawMode::Outline,
+        origin: origin
+      )
+
+      triangle = GSDL::Triangle.new(
+        x1: WIDTH / 2_f32 - 96,
+        y1: HEIGHT / 2_f32,
+        x2: WIDTH / 2_f32,
+        y2: HEIGHT / 2_f32 + 32,
+        x3: HEIGHT / 2_f32 + 32,
+        y3: HEIGHT / 2_f32 + 64,
+        color: GSDL::Color::White,
+        origin: origin
+      )
+      triangle.center(width: WIDTH, height: HEIGHT)
+      triangle.x -= 192
+      triangle.y += 128
+      @objects << triangle
+
+      line = GSDL::Line.new(
+        x1: WIDTH / 2_f32 - 96,
+        y1: HEIGHT / 2_f32 + 96,
+        x2: WIDTH / 2_f32 + 96,
+        y2: HEIGHT / 2_f32 + 96,
+      )
+      line.center(width: WIDTH, height: HEIGHT)
+      line.y += 96
+      @objects << line
+
+      @objects << GSDL::Pixel.new(
+        x: WIDTH / 2_f32,
+        y: HEIGHT / 2_f32 + 128
+      )
+
+      # add these last, so sprite is first, then shapes, then text
+      @objects << text
+      @objects << text_rotated
+    end
+
+    def active_object
+      @objects[@active_index]
+    end
+
+    def update(dt : Float32)
+      # Essential: call update on all objects so their tweens progress
+      @objects.each(&.update(dt))
+
+      if GSDL::Keys.just_pressed?(GSDL::Keys::Tab)
+        @active_index = (@active_index + 1) % @objects.size
+        name = active_object.class.name.split("::").last
+        @active_info.text = "Active: #{name}"
+      end
+
+      if GSDL::Keys.just_pressed?(GSDL::Keys::One)
+        active_object.tweens.clear
+        active_object.flash
+      end
+
+      if GSDL::Keys.just_pressed?(GSDL::Keys::Two)
+        active_object.tweens.clear
+        active_object.pulse
+      end
+
+      if GSDL::Keys.just_pressed?(GSDL::Keys::Three)
+        active_object.tweens.clear
+        active_object.spin
+      end
+
+      if GSDL::Keys.just_pressed?(GSDL::Keys::Four)
+        active_object.tweens.clear
+        active_object.shake
+      end
+    end
+
+    def draw(draw : GSDL::Draw)
+      @active_info.draw(draw)
+      @objects.each(&.draw(draw))
+    end
+  end
+
+  Game.new.run
+end
--- src/gsdl/core/tweenable.cr
+++ src/gsdl/core/tweenable.cr
@@ -24,6 +24,82 @@ module GSDL
       tweens.reject! { |t| !t.running? }
     end
 
+    def _tween_color_prop : String
+      self.is_a?(SpriteBase) ? "tint" : "color"
+    end
+
+    def flash(flash_color : Color = Color::Red, duration : Float32 = 0.1_f32, count : Int32 = 3)
+      t = tween
+      prop = _tween_color_prop
+      orig_color = prop == "tint" ? (tint || Color::White) : color
+
+      seq = [] of Hash(String, Tween::SequenceValue)
+      count.times do
+        seq << {"duration" => duration, prop => flash_color.as(Tween::SequenceValue)}
+        seq << {"duration" => duration, prop => orig_color.as(Tween::SequenceValue)}
+      end
+
+      t.add_sequence(seq)
+      t.start
+      t
+    end
+
+    def pulse(scale_factor : Float32 = 1.2_f32, duration : Float32 = 0.5_f32)
+      t = tween
+      orig_scale = self.scale
+
+      seq = [
+        {"duration" => duration / 2.0_f32, "scale" => {orig_scale[0].to_f32 * scale_factor, orig_scale[1].to_f32 * scale_factor}.as(Tween::SequenceValue), "easing" => "ease_out".as(Tween::SequenceValue)},
+        {"duration" => duration / 2.0_f32, "scale" => {orig_scale[0].to_f32, orig_scale[1].to_f32}.as(Tween::SequenceValue), "easing" => "ease_in".as(Tween::SequenceValue)}
+      ]
+
+      t.add_sequence(seq)
+      t.start
+      t
+    end
+
+    def spin(duration : Float32 = 1.0_f32, direction : Symbol = :clockwise)
+      t = tween
+      orig_rot = self.rotation.to_f32
+      target_rot = direction == :clockwise ? orig_rot + 360.0_f32 : orig_rot - 360.0_f32
+
+      seq = [
+        {"duration" => duration, "rotation" => target_rot.as(Tween::SequenceValue)}
+      ]
+
+      t.add_sequence(seq)
+      t.start
+      t
+    end
+
+    def shake(intensity : Float32 = 10.0_f32, duration : Float32 = 0.5_f32)
+      t = tween
+      orig_x = self.x.to_f32
+      orig_y = self.y.to_f32
+
+      steps = 10
+      step_duration = duration / steps
+
+      seq = [] of Hash(String, Tween::SequenceValue)
+      (steps - 1).times do
+        seq << {
+          "duration" => step_duration,
+          "x" => (orig_x + (rand - 0.5_f32) * intensity * 2).as(Tween::SequenceValue),
+          "y" => (orig_y + (rand - 0.5_f32) * intensity * 2).as(Tween::SequenceValue)
+        }
+      end
+
+      seq << {
+        "duration" => step_duration,
+        "x" => orig_x.as(Tween::SequenceValue),
+        "y" => orig_y.as(Tween::SequenceValue)
+      }
+
+      t.add_sequence(seq)
+      t.start
+      t
+    end
+
     abstract def tweens : Array(Tween)
 
     abstract def x : Num
@@ -40,15 +116,34 @@ module GSDL
     abstract def scale_y=(scale_y : Num)
 
     # Optional properties with default implementations
-    def offset_x : Num; 0_f32; end
+    def offset_x : Num
+      0_f32
+    end
+
     def offset_x=(val : Num); end
-    def offset_y : Num; 0_f32; end
+
+    def offset_y : Num
+      0_f32
+    end
+
     def offset_y=(val : Num); end
-    def rotation : Num; 0.0_f32; end
+
+    def rotation : Num
+      0.0_f32
+    end
+
     def rotation=(rotation : Num); end
-    def tint : Color?; nil; end
+
+    def tint : Color?
+      nil
+    end
+
     def tint=(val : Color?); end
-    def color : Color; Color::White; end
+
+    def color : Color
+      Color::White
+    end
+
     def color=(val : Color); end
   end
 end
