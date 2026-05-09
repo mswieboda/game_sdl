38d1474be24fa732419fe7cadc90f9e273258c93
Matt Swieboda <mswieboda@users.noreply.github.com>
2026-05-09 03:57:39 -0500

    removes test scale ex

--- examples/test_scale.cr
+++
@@ -1,9 +0,0 @@
-require "../src/game_sdl"
-
-class ScaleTest < GSDL::Game
-  def init
-    puts "Display scale: #{ScaleTest.draw.content_scale}"
-  end
-end
-
-ScaleTest.new(title: "Scale Test", high_pixel_density: true).run
